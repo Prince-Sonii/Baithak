c7e76c6c803021e30c7ab7b6a16f35e36afc0f90
prince <princesoni200325@gmail.com>
2024-06-30 18:34:04 +0530

    Added functionality to message wihtin the collaboration

---
+++ baithak.js
@@ -0,0 +1,12 @@
+let message;
+const displayMessage = document.getElementById("msg_screen");
+const typingArea = document.getElementById("typing_area");
+const sendButton = document.getElementById("send_msg");
+sendButton.addEventListener("click",()=>{
+    message = typingArea.value;
+    typingArea.value="";
+    const outerBox = document.createElement("p");
+    outerBox.className = "user_message";
+    outerBox.innerText = message;
+    displayMessage.prepend(outerBox);  
+})
